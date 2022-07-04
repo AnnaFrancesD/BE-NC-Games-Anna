3eacc9de6a330b6db0585175ee8a26aee4fe74e5
Anna Dehl <annadehl@Annas-MacBook-Air.local>
2022-07-04 11:15:53 +0100

    write test for get categories

--- __tests__/endpoints.test.js
+++ __tests__/endpoints.test.js
@@ -5,3 +5,24 @@ const connection = require("../db/connection");
 afterAll(() => {
   return connection.end();
 });
+
+describe("app", () => {
+  describe("GET /api/categories", () => {
+    test("status 200, responds with an array of category objects", () => {
+      return request(app)
+        .get("/api/categories")
+        .expect(200)
+        .then(({ body: { categories } }) => {
+          expect(categories).toBeInstanceOf(Array);
+          categories.forEach((category) => {
+            expect(category).toEqual(
+              expect.objectContaining({
+                slug: expect.any(String),
+                description: expect.any(String),
+              })
+            );
+          });
+        });
+    });
+  });
+});
