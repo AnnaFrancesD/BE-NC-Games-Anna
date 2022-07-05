fff6f3fc8b6606e77af276b88b924f87ff5b80a5
Anna Dehl <annadehl@Annas-MacBook-Air.local>
2022-07-05 16:05:47 +0100

    remove unnecessary table name change in get reviews model

--- models/reviews-models.js
+++ models/reviews-models.js
@@ -40,10 +40,10 @@ exports.fetchReviews = () => {
   return connection
     .query(
       `
-  SELECT a.*, COUNT (b.review_id) AS comment_count FROM reviews a
-  LEFT JOIN comments b
-  ON a.review_id = b.review_id
-  GROUP BY a.review_id
+  SELECT reviews.*, COUNT (comments.review_id) AS comment_count FROM reviews
+  LEFT JOIN comments
+  ON reviews.review_id = comments.review_id
+  GROUP BY reviews.review_id
   ORDER BY created_at DESC;`
     )
     .then(({ rows }) => {
